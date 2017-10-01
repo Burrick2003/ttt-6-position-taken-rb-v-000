071ba5f3c8415f62ceb473dbac4728e4723a7b6f
Blakeley Bond <burrick2003@yahoo.com>
2017-10-01 04:48:08 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if board[index] == "X" || board[index] == "Y"
-    true
-  elsif board[index] == " " || board[index] == ""
     false
+  elsif board[index] == " " || board[index] == ""
+    true
   end
 end
